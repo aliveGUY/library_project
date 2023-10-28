c85a429ac85133ed04d443d58f5e6edcdbe564b1
illdub <illia.dubrovin@dreamhost.com>
2023-10-28 21:46:36 +0300

    edit layout

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -24,21 +24,26 @@ private:
     void OnTimer(wxTimerEvent &event); // Timer event handler
 
     wxTimer messageTimer;
+    wxStaticText *pingText;
+    wxStaticText *messageText;
 };
 
 MyFrame::MyFrame() : wxFrame(nullptr, wxID_ANY, "Hello World")
 {
     wxPanel *panel = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize);
-    panel->SetBackgroundColour(wxColor(100, 100, 200));
     wxBoxSizer *sizer = new wxBoxSizer(wxVERTICAL);
     sizer->Add(panel, 1, wxEXPAND | wxALL, 5);
 
     wxButton *pingButton = new wxButton(panel, wxID_ANY, "Ping");
-    wxButton *messageButton = new wxButton(panel, wxID_ANY, "Ping");
+    wxButton *messageButton = new wxButton(panel, wxID_ANY, "Message");
+    pingText = new wxStaticText(panel, wxID_ANY, "*empty*");
+    messageText = new wxStaticText(panel, wxID_ANY, "*empty*");
     wxSizer *elemSizer = new wxBoxSizer(wxVERTICAL);
 
     elemSizer->Add(pingButton, 0, wxALIGN_CENTER_HORIZONTAL);
+    elemSizer->Add(pingText, 0, wxALIGN_CENTER_HORIZONTAL, 0);
     elemSizer->Add(messageButton, 0, wxALIGN_CENTER_HORIZONTAL);
+    elemSizer->Add(messageText, 0, wxALIGN_CENTER_HORIZONTAL, 0);
 
     Connect(pingButton->GetId(), wxEVT_BUTTON, wxCommandEventHandler(MyFrame::OnPing));
     Connect(messageButton->GetId(), wxEVT_BUTTON, wxCommandEventHandler(MyFrame::MessageAll));
@@ -47,7 +52,7 @@ MyFrame::MyFrame() : wxFrame(nullptr, wxID_ANY, "Hello World")
     this->SetSizerAndFit(sizer);
 
     messageTimer.Bind(wxEVT_TIMER, &MyFrame::OnTimer, this); // Bind timer event to OnTimer method
-    messageTimer.Start(1000); // Adjust the interval as needed
+    messageTimer.Start(1000);                                // Adjust the interval as needed                            // Start the timer (adjust the interval as needed)
 }
 
 void MyFrame::OnPing(wxCommandEvent &event)
@@ -60,7 +65,6 @@ void MyFrame::OnPing(wxCommandEvent &event)
 
     msg << timeNow;
     Send(msg);
-    wxMessageBox("Ping", "Png", wxOK | wxICON_INFORMATION);
 }
 
 void MyFrame::MessageAll(wxCommandEvent &event)
@@ -68,7 +72,6 @@ void MyFrame::MessageAll(wxCommandEvent &event)
     olc::net::message<CustomMsgTypes> msg;
     msg.header.id = CustomMsgTypes::MessageAll;
     Send(msg);
-    wxMessageBox("Message all", "Message all", wxOK | wxICON_INFORMATION);
 }
 
 void MyFrame::OnTimer(wxTimerEvent &event)
@@ -97,6 +100,8 @@ void MyFrame::CheckForMessages()
             std::chrono::system_clock::time_point timeThen;
             msg >> timeThen;
             std::cout << "Ping: " << std::chrono::duration<double>(timeNow - timeThen).count() << "\n";
+
+            pingText->SetLabel(std::to_string(std::chrono::duration<double>(timeNow - timeThen).count()));
         }
         break;
 
@@ -106,6 +111,7 @@ void MyFrame::CheckForMessages()
             uint32_t clientID;
             msg >> clientID;
             std::cout << "Hello from [" << clientID << "]\n";
+            messageText->SetLabel("Hello from [" + std::to_string(clientID) + "]");
         }
         break;
         }
